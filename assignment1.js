0e7212092d3b7be040c3eccc0b7546b8efee3e5c
chucktagg83 <bgtagg@gmail.com>
2026-01-28 17:46:25 -0700

    variables

---
+++ assignment1.js
@@ -0,0 +1,26 @@
+let character = "Ron";
+let action = "run";
+let place = "school";
+let object = "basketball";
+const time = "morning";
+
+
+// 2. Create the Story
+console.log("---Funny Story---");
+console.log(character + " goes for a " + action + " every " + time + ".")
+console.log("Suddenly " + character + " saw a " + object + ", and life changed.")
+console.log("Now, " + object + " was a must, every " + time + " before " + place + ".")
+
+// 3. Experiment with different story
+console.log("---Different Version of the Story---")
+
+// Change values to create a different story
+character = "Sarah";
+action = "walk";
+place = "work";
+object = "frog chasing";
+
+console.log(character + " goes for a " + action + " every " + time + ".")
+console.log("Suddenly " + character + " saw a " + object + ", and life changed.")
+console.log("Now, " + object + " was a must, every " + time + " before " + place + ".")
+
---
+++ scripts/variables.js
@@ -0,0 +1,66 @@
+
+
+console.log("Variables");
+
+// String
+let firstName = "Chuck";
+// numerical
+let myAge = 99;
+// boolean
+let isProfessor = false;
+let isStudent = true;
+
+//dispaly the values
+console.log(myAge)
+console.log(firstName)
+console.log(isProfessor)
+console.log(isStudent)
+
+
+// Mini Challenge //
+let mylastName = "Taggart";
+let myHeight = "6'1";
+
+console.log(mylastName)
+console.log(myHeight)
+
+/*-----Arithmetic Oepration:*/
+let num1 = 10;
+let num2 = 20; 
+
+let sum = num1 + num2;
+let subs = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+
+console.log(sum);
+
+// concatenation
+//console.log (num1 + "+" + num2 + num2 + "=" + sum) //
+console.log(`${num1} + ${num2} = ${sum}`);
+console.log(`${num1} - ${num2} = ${subs}`);
+console.log(`${num1} x ${num2} = ${mult}`);
+console.log(`${num1} / ${num2} = ${div}`);
+
+//calcultae the area of circle with ${radius}
+
+
+let pi = 3.1416; 
+let radius = 3;
+let circleArea = pi * radius * radius;
+
+console.log(`The area of a circle with ${radius} is ${circleArea}`);
+
+// difference between variable and constant
+let aVariable; //create a variable
+aVariable = 20; // assign a value
+aVariable = 3
+
+// create and assign value
+let anotherVariable = 30;
+
+console.log(aVariable);
+
+const weekDays = 7
+//weekDays = 9;
+console.log(weekDays);
